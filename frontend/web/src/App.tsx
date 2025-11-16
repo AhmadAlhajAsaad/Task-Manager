cee2f7d79f66ee88dd6ec5be03458eba5d815151
Ahmad Alhaj Asaad <ahmadalhajasaad1982@gmail.com>
2025-11-16 19:31:10 +0100

    Add React + TypeScript frontend (web project)

---
+++ frontend/web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Task Manager</h1>
+      <p>Frontend with React + TypeScript</p>
+    </div>
+  );
+}
+
+export default App;
